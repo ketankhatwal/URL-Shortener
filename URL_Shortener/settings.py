98f1484b9d0437c5a7a9f856ddc1b5cf7430f487
Ketan Khatwal <Ketan.Khatwal@gmail.com>
2020-02-29 10:07:23 +0530

    Updated settings

--- URL_Shortener/settings.py
+++ URL_Shortener/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*c^45(&m(bidbk=9gn-b8sdadmyv*+1(oo(9(6y6^))7(&6od3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uvshort.herokuapp.com']
 
 
 # Application definition
